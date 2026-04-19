8e647bfc60c6e8ad3194c446689b575138c37396
David Escarcega <davidaescarcega@gmail.com>
2026-04-19 12:34:16 -0700

    eliminacion de seguimiento

---
+++ app-ui/src/main/java/frames/LogInFrame.java
@@ -0,0 +1,14 @@
+package frames;
+
+import javax.swing.*;
+
+public class FrameLogIn extends JFrame {
+
+
+    public FrameLogIn() {
+        setTitle("Log In");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
