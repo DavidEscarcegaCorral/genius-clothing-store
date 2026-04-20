726f5f9c42de06108b247d9ad7597c2c11c420b5
David Escarcega <davidaescarcega@gmail.com>
2026-04-20 11:51:12 -0700

    header colores

--- app-ui/src/main/java/panels/Header.java
+++ app-ui/src/main/java/panels/Header.java
@@ -1,9 +1,29 @@
 package panels;
 
+import componentes.LogoGenius;
+import util.Estilo;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class Header extends JPanel {
+    private JPanel panelSuperior;
+    private LogoGenius homeButton;
 
+    public Header(){
+        setBackground(Color.BLACK);
+
+        // Layout
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        panelSuperior = new JPanel();
+
+        // Borde amarillo
+        setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, Estilo.AMARILLO_GENIUS));
+
+
+    }
 
 
 }
--- app-ui/src/main/java/util/Estilo.java
+++ app-ui/src/main/java/util/Estilo.java
@@ -5,5 +5,8 @@ import java.awt.*;
 public class Estilo {
 
     // Colores
-    public static final Color AmarilloGenius = new Color(255, 255, 100);
+    public static final Color AMARILLO_GENIUS = new Color(255, 255, 100);
+
+    // Fonts
+    public static final Font FONT_PRGRAMME_NORMAL = new FontLoader().cargarFont("programme_normal.ttf", 24f);
 }
