22c42a04b66e0b20690ff1c184cf3fa3ef9454bc
David Escarcega <davidaescarcega@gmail.com>
2026-04-29 13:16:53 -0700

    card de producto mock terminada

--- app-ui/src/main/java/util/Estilo.java
+++ app-ui/src/main/java/util/Estilo.java
@@ -14,4 +14,6 @@ public class Estilo {
     public static final String FONT_PROGRAMME_LIGHT = "programme_light.ttf";
     public static final String FONT_OPNS_REGULAR = "OpenSans-Regular.ttf";
     public static final String FONT_OPNS_COND_REGULAR = "OpenSans_Condensed-Regular.ttf";
+    public static final String FONT_OPNS_COND_BLOD = "OpenSans_Condensed-Bold.ttf";
+
 }
