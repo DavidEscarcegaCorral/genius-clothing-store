7898b2f7b8850b4f7e25f7f5f4199a84b1cd794d
Angel1373 <angel.gaxiolap3@gmail.com>
2026-05-02 12:53:40 -0700

    Base de producto
    Creación base de pantallas que se utilizaran para el caso de producto

---
+++ app-ui/src/main/java/frames/AdministracionProductoFrame.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frames;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import panels.AdministracionProductosPanel;
+import panels.Header;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AdministracionProductoFrame extends JFrame {
+   
+    public AdministracionProductosPanel administracionProductosPanel;
+
+    public AdministracionProductoFrame() {
+        
+        setTitle("Menu de productos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().setBackground(Color.white);
+        setMinimumSize(new Dimension(300,200));
+        
+        administracionProductosPanel = new AdministracionProductosPanel();
+        add(administracionProductosPanel);
+        
+        //Centramos los componentes
+        pack();
+        //Centramos la pantalla
+        setLocationRelativeTo(null);
+    }
+
+    public AdministracionProductosPanel getAdministracionProductosPanel() {
+        return administracionProductosPanel;
+    }
+    
+    
+    
+}
