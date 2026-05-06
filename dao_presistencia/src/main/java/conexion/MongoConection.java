88153f63d70e08d4bf24fce536c80fab79e9a16e
Angel1373 <angel.gaxiolap3@gmail.com>
2026-05-06 12:36:59 -0700

    Arreglo de errores y Conexion a Mongo
    Cambios en el dto de producto por que cambie el id de producto por string en vez de ObjectiD

--- dao_presistencia/src/main/java/conexion/MongoConection.java
+++ dao_presistencia/src/main/java/conexion/MongoConection.java
@@ -4,10 +4,38 @@
  */
 package conexion;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 /**
  *
  * @author Usuario
  */
 public class MongoConection {
+    
+    //URL
+    private static final String URL = "mongodb://localhost:27017";
+    //Base de datos
+    private static final String BASE_DATOS = "genius-clothing-store";
+    
+    private static MongoClient cliente;
+    private MongoConection(){
+        
+    }
+    //Singleton que recibe el cliente si existe y si no la crea junto con la conexion de Mongo
+    //Crea la conexion con la base de datos
+    private static MongoClient obtenerCliente(){
+        if(cliente == null){
+            cliente = MongoClients.create(URL);
+        }
+        return cliente;
+    }
+    
+    //Obtiene la base de datos
+    public static MongoDatabase obtenerBaseDatos(){
+        return obtenerCliente().getDatabase(BASE_DATOS);
+    }
+    
     
 }
