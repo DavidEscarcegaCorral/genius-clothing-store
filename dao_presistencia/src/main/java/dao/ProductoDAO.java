7f1aa539513201d876fb54f3231f9e32710a70ae
David Escarcega <davidaescarcega@gmail.com>
2026-05-09 09:50:08 -0700

    ConexionMongo

--- dao_presistencia/src/main/java/dao/ProductoDAO.java
+++ dao_presistencia/src/main/java/dao/ProductoDAO.java
@@ -7,67 +7,70 @@ package dao;
 import adapters.ProductoPersistenciaAdapter;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoCollection;
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.set;
 import com.mongodb.client.result.UpdateResult;
-import conexion.MongoConection;
+import conexion.ConexionMongoDB;
 import dominio.ProductoEntidad;
 import entidadesmongo.ProductoMongoEntidad;
 import enumeradores.EstadoProducto;
 import excepciones.PersistenciaException;
+import org.bson.types.ObjectId;
+
 import java.util.ArrayList;
 import java.util.List;
-import org.bson.types.ObjectId;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.set;
 
 /**
  *
  * @author Usuario
  */
-public class ProductoDAO implements IProductoDAO{
-    
+public class ProductoDAO implements IProductoDAO {
+
     private final MongoCollection<ProductoMongoEntidad> coleccionProductos;
     private final ProductoPersistenciaAdapter productoAdapter;
 
     public ProductoDAO() {
-        this.coleccionProductos = MongoConection.obtenerColeccionProductos();
+        this.coleccionProductos = ConexionMongoDB.getInstance()
+                .getCollection("productos", ProductoMongoEntidad.class);
         this.productoAdapter = new ProductoPersistenciaAdapter();
     }
 
     @Override
-    public ProductoEntidad agregarProducto(ProductoEntidad producto)throws PersistenciaException {
-        
-        if(producto == null){
+    public ProductoEntidad agregarProducto(ProductoEntidad producto) throws PersistenciaException {
+
+        if (producto == null) {
             throw new PersistenciaException("El producto no puede ser nulo");
         }
-        try{
+        try {
             ProductoMongoEntidad mongoProducto = productoAdapter.convertirAMongo(producto);
-            if(mongoProducto.getId()==null){
+            if (mongoProducto.getId() == null) {
                 mongoProducto.setId(new ObjectId());
             }
-            coleccionProductos.insertOne(mongoProducto);            
+            coleccionProductos.insertOne(mongoProducto);
             return productoAdapter.convertirADominio(mongoProducto);
-            
-        }catch(MongoException e){
-            throw new PersistenciaException("Error al intentar guardar el producto",e);
+
+        } catch (MongoException e) {
+            throw new PersistenciaException("Error al intentar guardar el producto", e);
         }
     }
 
     @Override
     public ProductoEntidad cambiarEstado(String id, EstadoProducto estado) throws PersistenciaException {
-        
+
         ObjectId idObject = productoAdapter.convertirStringAObjectId(id);
-        if(idObject == null){
+        if (idObject == null) {
             throw new PersistenciaException("El producto con ese id no existe");
         }
-        try{
+        try {
             //UpdateResult es por que update one lo regresa 
             //"estado" por que el campo del ProductoMongoEntidad se llama asi
-            UpdateResult resultado = coleccionProductos.updateOne(eq("_id",idObject),set("estado",estado));
-            
-            ProductoMongoEntidad mongoProducto = coleccionProductos.find(eq("_id",idObject)).first();
+            UpdateResult resultado = coleccionProductos.updateOne(eq("_id", idObject), set("estado", estado));
+
+            ProductoMongoEntidad mongoProducto = coleccionProductos.find(eq("_id", idObject)).first();
 
             return productoAdapter.convertirADominio(mongoProducto);
-        }catch(MongoException e){
+        } catch (MongoException e) {
             throw new PersistenciaException("Error al cambiar el estado del producto");
         }
     }
@@ -75,42 +78,42 @@ public class ProductoDAO implements IProductoDAO{
     @Override
     public ProductoEntidad publicarProducto(String id) throws PersistenciaException {
         ObjectId idObject = productoAdapter.convertirStringAObjectId(id);
-        if(idObject == null){
+        if (idObject == null) {
             throw new PersistenciaException("El producto con ese id no existe");
         }
-        try{
-            UpdateResult resultado = coleccionProductos.updateOne(eq("_id",idObject),set("estado",EstadoProducto.PUBLICADO));
-            ProductoMongoEntidad mongoProducto = coleccionProductos.find(eq("_id",idObject)).first();
+        try {
+            UpdateResult resultado = coleccionProductos.updateOne(eq("_id", idObject), set("estado", EstadoProducto.PUBLICADO));
+            ProductoMongoEntidad mongoProducto = coleccionProductos.find(eq("_id", idObject)).first();
             return productoAdapter.convertirADominio(mongoProducto);
-        }catch(MongoException e){
+        } catch (MongoException e) {
             throw new PersistenciaException("Error al publicar el producto");
         }
-        
+
     }
 
     @Override
     public ProductoEntidad buscarPorId(String id) throws PersistenciaException {
         ObjectId idObject = productoAdapter.convertirStringAObjectId(id);
-        
-        if(idObject == null){
+
+        if (idObject == null) {
             throw new PersistenciaException("El restaurante con ese id no existe");
         }
-        try{
-        ProductoMongoEntidad mongoProducto = coleccionProductos.find(eq("_id",idObject)).first();
-        return productoAdapter.convertirADominio(mongoProducto);
-        
-        }catch(MongoException e){
+        try {
+            ProductoMongoEntidad mongoProducto = coleccionProductos.find(eq("_id", idObject)).first();
+            return productoAdapter.convertirADominio(mongoProducto);
+
+        } catch (MongoException e) {
             throw new PersistenciaException("Error al buscar el producto por id");
         }
     }
 
     @Override
     public List<ProductoEntidad> obtenerProductos() throws PersistenciaException {
-        try{
+        try {
             //Todos los resultados los convierte a una lista
             List<ProductoMongoEntidad> mongoProductoLista = coleccionProductos.find().into(new ArrayList<>());
             return productoAdapter.convertirListaADominio(mongoProductoLista);
-        }catch(MongoException e){
+        } catch (MongoException e) {
             throw new PersistenciaException("Error al ver todos los productos");
         }
     }
